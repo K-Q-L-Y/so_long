67a5f912cb3e8618f00618e3f4d87b380e61fe3d
Choongsk_wsl <sengkhye@gmail.com>
2024-08-15 23:31:10 +0800

    fixed nl

--- srcs/init/ft_strlen_nl.c
+++ srcs/init/ft_strlen_nl.c
@@ -6,7 +6,7 @@
 /*   By: cseng-kh <cseng-kh@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 22:05:13 by cseng-kh          #+#    #+#             */
-/*   Updated: 2024/08/15 22:52:06 by cseng-kh         ###   ########.fr       */
+/*   Updated: 2024/08/15 22:59:47 by cseng-kh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strlen_nl(char *s)
 	len = ft_strlen(s);
 	if (!len)
 		return (0);
-	while ((len > 0) & (s[len - 1] == '\n' || s[len - 1] == '\r'))
+	while ((len > 0) && (s[len - 1] == '\n' || s[len - 1] == '\r'))
 		len -= 1;
 	return (len);
 }
